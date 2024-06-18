be64c926716f365ce11479b6389fdddfe0391cc0
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-18 15:20:51 -0400

    Adding a Scrollable Widget.

--- lib/screens/create/create.dart
+++ lib/screens/create/create.dart
@@ -52,77 +52,79 @@ class _CreateState extends State<Create> {
           vertical: 30,
           horizontal: 20,
         ),
-        child: Column(
-          children: [
-            // Welcome message
-            Center(
-              child: Icon(
-                Icons.code,
-                color: AppColors.primaryColor,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Welcome message
+              Center(
+                child: Icon(
+                  Icons.code,
+                  color: AppColors.primaryColor,
+                ),
               ),
-            ),
-            const Center(
-              child: StyledHeading('Welcome, new player.'),
-            ),
-            const Center(
-              child: StyledText('Create a name & slogan for your character.'),
-            ),
-            const SizedBox(height: 30),
+              const Center(
+                child: StyledHeading('Welcome, new player.'),
+              ),
+              const Center(
+                child: StyledText('Create a name & slogan for your character.'),
+              ),
+              const SizedBox(height: 30),
 
-            // input for name and slogan
-            TextField(
-              controller: _nameController,
-              style: GoogleFonts.kanit(
-                textStyle: Theme.of(context).textTheme.bodyMedium,
+              // input for name and slogan
+              TextField(
+                controller: _nameController,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.person_2),
+                  label: StyledText('Charater name'),
+                ),
               ),
-              cursorColor: AppColors.textColor,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.person_2),
-                label: StyledText('Charater name'),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _sloganController,
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.bodyMedium,
+                ),
+                cursorColor: AppColors.textColor,
+                decoration: const InputDecoration(
+                  prefixIcon: Icon(Icons.chat),
+                  label: StyledText('Charater slogan'),
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            TextField(
-              controller: _sloganController,
-              style: GoogleFonts.kanit(
-                textStyle: Theme.of(context).textTheme.bodyMedium,
-              ),
-              cursorColor: AppColors.textColor,
-              decoration: const InputDecoration(
-                prefixIcon: Icon(Icons.chat),
-                label: StyledText('Charater slogan'),
-              ),
-            ),
-            const SizedBox(height: 30),
+              const SizedBox(height: 30),
 
-            // Select vocation title
-            Center(
-              child: Icon(
-                Icons.code,
-                color: AppColors.primaryColor,
+              // Select vocation title
+              Center(
+                child: Icon(
+                  Icons.code,
+                  color: AppColors.primaryColor,
+                ),
               ),
-            ),
-            const Center(
-              child: StyledHeading('Choose a vocation.'),
-            ),
-            const Center(
-              child: StyledText('This determines your available skills.'),
-            ),
-            const SizedBox(height: 30),
-
-            // Vocation cards
-            const VocationCard(vocation: Vocation.junkie),
-            const VocationCard(vocation: Vocation.ninja),
-            const VocationCard(vocation: Vocation.raider),
-            const VocationCard(vocation: Vocation.wizard),
-
-            Center(
-              child: StyledButton(
-                onPressed: handleSubmit,
-                child: const StyledHeading('Create character'),
+              const Center(
+                child: StyledHeading('Choose a vocation.'),
               ),
-            ),
-          ],
+              const Center(
+                child: StyledText('This determines your available skills.'),
+              ),
+              const SizedBox(height: 30),
+
+              // Vocation cards
+              const VocationCard(vocation: Vocation.junkie),
+              const VocationCard(vocation: Vocation.ninja),
+              const VocationCard(vocation: Vocation.raider),
+              const VocationCard(vocation: Vocation.wizard),
+
+              Center(
+                child: StyledButton(
+                  onPressed: handleSubmit,
+                  child: const StyledHeading('Create character'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
