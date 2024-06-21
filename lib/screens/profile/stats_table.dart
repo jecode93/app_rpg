e65289a392895dfcbdcd222a091a01b9ae613463
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-21 16:02:16 -0400

    Using Tables.

--- lib/screens/profile/stats_table.dart
+++ lib/screens/profile/stats_table.dart
@@ -38,9 +38,66 @@ class _StatsTableState extends State<StatsTable> {
                 StyledHeading(widget.character.points.toString()),
               ],
             ),
-          )
+          ),
 
           // Stats table
+          Table(
+            children: widget.character.statsAsFormattedList.map((stat) {
+              return TableRow(
+                decoration: BoxDecoration(
+                  color: AppColors.secondaryColor.withOpacity(0.5),
+                ),
+                children: [
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: StyledHeading(stat['title']!),
+                    ),
+                  ),
+
+                  // stat value
+                  TableCell(
+                    verticalAlignment: TableCellVerticalAlignment.middle,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: StyledHeading(stat['value']!),
+                    ),
+                  ),
+
+                  // Icon to increase stat
+                  TableCell(
+                      verticalAlignment: TableCellVerticalAlignment.middle,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_upward,
+                          color: AppColors.textColor,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            widget.character.increaseStat(stat['title']!);
+                          });
+                        },
+                      )),
+
+                  // Icon to increase stat
+                  TableCell(
+                      verticalAlignment: TableCellVerticalAlignment.middle,
+                      child: IconButton(
+                        icon: Icon(
+                          Icons.arrow_downward,
+                          color: AppColors.textColor,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            widget.character.decreaseStat(stat['title']!);
+                          });
+                        },
+                      )),
+                ],
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
