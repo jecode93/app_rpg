6364f1a73cf634612fa68c9f2f7344944ad8c395
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-21 13:38:53 -0400

    Passing Data into Routes.

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,28 +1,55 @@
+import 'package:app_rpg/models/character.dart';
 import 'package:app_rpg/shared/styled_text.dart';
+import 'package:app_rpg/theme.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
-  const Profile({super.key});
+  const Profile({
+    super.key,
+    required this.character,
+  });
+
+  final Character character;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const StyledHeading('Character name'),
+        title: StyledHeading(character.name),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             // Basic info - image, vocation, description
-            
-            
+            Container(
+              padding: const EdgeInsets.all(16),
+              color: AppColors.secondaryColor.withOpacity(0.3),
+              child: Row(
+                children: [
+                  Image.asset(
+                    'assets/img/vocations/${character.vocation.image}',
+                    width: 140,
+                    height: 140,
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StyledHeading(character.vocation.title),
+                        StyledText(character.vocation.description),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
             // Weapon and ability
-            
-            
+
             // Stats & skills
-            
-            
-            // Save button 
+
+            // Save button
           ],
         ),
       ),
