5440e8f09df1cc276568b19781f1c488ed7285fe
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-06-21 13:59:08 -0400

    Fleshing Out the Profile Screen.

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -45,8 +45,39 @@ class Profile extends StatelessWidget {
                 ],
               ),
             ),
-            // Weapon and ability
 
+            // Weapon, ability and slogan
+            const SizedBox(
+              height: 20,
+            ),
+            Center(
+              child: Icon(
+                Icons.code,
+                color: AppColors.primaryColor,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(16),
+                color: AppColors.secondaryColor.withOpacity(0.5),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const StyledHeading('Slogan'),
+                    StyledText(character.slogan),
+                    const SizedBox(height: 10),
+                    const StyledHeading('Weapon of Choice'),
+                    StyledText(character.vocation.weapon),
+                    const SizedBox(height: 10),
+                    const StyledHeading('Unique Ability'),
+                    StyledText(character.vocation.ability),
+                    const SizedBox(height: 10)
+                  ],
+                ),
+              ),
+            )
             // Stats & skills
 
             // Save button
