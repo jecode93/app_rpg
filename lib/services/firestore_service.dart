50d6fb790042dc475cb716fa3ae3e2c70b9f6528
Jean Emmanuel Cadet <jeanemmanuelcadet@gmail.com>
2024-07-05 13:16:52 -0400

    Firestore Converters.

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,5 +1,10 @@
+import 'package:app_rpg/models/character.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class FirestoreService {
-  static final ref = FirebaseFirestore.instance.collection('characters');
+  static final ref =
+      FirebaseFirestore.instance.collection('characters').withConverter(
+            fromFirestore: Character.fromFirestore,
+            toFirestore: (Character c, _) => c.toFirestore(),
+          );
 }
